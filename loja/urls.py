11c1fed324374d3f6f211af140615283635f642c
cwschopsjr <cwschops.jr@gmail.com>
2024-11-12 16:00:21 -0300

    Adicionei o CSS e os templates

--- loja/urls.py
+++ loja/urls.py
@@ -1,8 +1,10 @@
-from loja.views import index
+from loja.views import index, page, post
 from django.urls import path
 
 app_name = 'loja'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,10 +1,46 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+posts = list(range(1000))
 
-from django.shortcuts import render
+
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'loja/pages/index.html'
+        'loja/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'loja/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
+
+
+def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'loja/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
