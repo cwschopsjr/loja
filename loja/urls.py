af4ef77d341a54ac43b5da21679f043dfb9fcb5c
cwschopsjr <cwschops.jr@gmail.com>
2024-11-13 16:21:17 -0300

    Ajustando as views

--- loja/urls.py
+++ loja/urls.py
@@ -1,4 +1,4 @@
-from loja.views import PostListView, page, post, category, created_by, tag, search
+from loja.views import TagListView, CategoryListView, CreatedByListView, PostListView, page, post, search
 from django.urls import path
 
 app_name = 'loja'
@@ -8,8 +8,12 @@ urlpatterns = [
     path('post/', post, name='post'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
+    path(
+        'created_by/<int:author_pk>/',
+        CreatedByListView.as_view(),
+        name='created_by'
+    ),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
     path('search/', search, name='search'),
 ]
